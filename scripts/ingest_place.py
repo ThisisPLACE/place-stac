ddbb0d8765f5ec6846b7ca5e89384067ebf77bf3
Nathan Zimmerman <npzimmerman@gmail.com>
2022-09-16 10:05:02 -0500

    Set up development environment

---
+++ scripts/ingest_place.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+import orjson
+from pypgstac.load import Loader, Methods, PgstacDB
+
+DATA_DIR =os.path.join(os.path.join(os.path.dirname(__file__), ".."), "data")
+collection = os.path.join(DATA_DIR, "test_collection.json")
+items = os.path.join(DATA_DIR, "items")
+
+
+def load_test_data() -> None:
+    with PgstacDB() as conn:
+        loader = Loader(db=conn)
+        with open(collection, "rb") as f:
+            c = orjson.loads(f.read())
+            loader.load_collections([c], Methods.upsert)
+        pathlist = Path(items).glob("*.json")
+        for path in pathlist:
+            with open(str(path), "rb") as f:
+                i = orjson.loads(f.read())
+                loader.load_items([i], Methods.upsert)
+
+
+load_test_data()
